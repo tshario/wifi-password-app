29d57376c8474212e900498ab367ba32238a3ab8
tshario <33377998+tshario@users.noreply.github.com>
2017-11-05 14:52:26 +0800

    Show Wi-Fi name and password

--- wifi-password-app/wifi-password-app/AppDelegate.m
+++ wifi-password-app/wifi-password-app/AppDelegate.m
@@ -11,23 +11,31 @@
 #import "SAMKeychain.h"
 
 @interface AppDelegate ()
-
 @property (weak) IBOutlet NSWindow *window;
 
+@property (weak) IBOutlet NSTextField *accountLabel;
+@property (weak) IBOutlet NSTextField *passwordLabel;
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    self.window.styleMask = NSTitledWindowMask | NSClosableWindowMask | NSMiniaturizableWindowMask;
+    
     CWInterface *wifi = [[CWWiFiClient sharedWiFiClient] interface];
     NSString *account = wifi.ssid;
     NSString *service = @"AirPort";
     NSString *password = [SAMKeychain passwordForService:service account:account];
-    NSLog(@"%@", password);
+    
+    account = account ?: @"ERROR";
+    password = password ?: @"ERROR";
+    
+    self.accountLabel.stringValue = [NSString stringWithFormat:@"Wi-Fi: %@", account];
+    self.passwordLabel.stringValue = [NSString stringWithFormat:@"Password: %@", password];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+    
 }
 
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
