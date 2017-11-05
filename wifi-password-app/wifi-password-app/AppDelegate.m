68d7861c4fd40efe66f306b8eefa7e9edf6a603b
tshario <33377998+tshario@users.noreply.github.com>
2017-11-05 13:10:05 +0800

    Get password from Keychain

--- wifi-password-app/wifi-password-app/AppDelegate.m
+++ wifi-password-app/wifi-password-app/AppDelegate.m
@@ -7,22 +7,31 @@
 //
 
 #import "AppDelegate.h"
+#import <CoreWLAN/CoreWLAN.h>
+#import "SAMKeychain.h"
 
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+
 @end
 
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    CWInterface *wifi = [[CWWiFiClient sharedWiFiClient] interface];
+    NSString *account = wifi.ssid;
+    NSString *service = @"AirPort";
+    NSString *password = [SAMKeychain passwordForService:service account:account];
+    NSLog(@"%@", password);
 }
-
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
     // Insert code here to tear down your application
 }
 
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)sender {
+    return YES;
+}
 
 @end
