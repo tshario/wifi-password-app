0ef756652689f20c5996013dd177c6ef93799bd6
tshario <33377998+tshario@users.noreply.github.com>
2017-11-05 12:41:51 +0800

    .

---
+++ wifi-password-app/wifi-password-app/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  wifi-password-app
+//
+//  Created by tshario on 05/11/2017.
+//  Copyright © 2017 tshario. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ wifi-password-app/wifi-password-app/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  wifi-password-app
+//
+//  Created by tshario on 05/11/2017.
+//  Copyright © 2017 tshario. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
